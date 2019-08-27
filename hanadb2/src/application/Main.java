db4498bf54236f6cef2beb895d82b367116c0bb4
zjambor <jambor.zoltan1@gmail.com>
2019-08-27 13:10:45 +0200

    ok

--- hanadb2/src/application/Main.java
+++ hanadb2/src/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("SAP HANA Database Monitoring");
+		this.primaryStage.setTitle("SAP HANA Database Monitoring 500");
 
 		// connect();
 		initRootLayout();
